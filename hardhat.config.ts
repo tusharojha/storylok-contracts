88dde424ae545d331b0489520bf14cdf0c9a7fc6
Tushar Ojha <tusharojha2001@gmail.com>
2023-10-21 08:56:32 +0530

    deployed contract and added script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
+
+    avaxTestnet: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY ?? ''],
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
